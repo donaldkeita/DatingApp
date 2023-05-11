c89e0d485c5a6d09c33ae47f220a833645098f6e
donaldkeita <keitadonald@gmail.com>
2023-05-11 12:56:56 -0400

    Paging, Sorting and Filtering - Adding sorting functionality

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -43,6 +43,12 @@ namespace API.Data
 
             query = query.Where(u => u.DateOfBirth >= minDobDateTime && u.DateOfBirth <= maxDobDateTime);
 
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+
             return await PagedList<MemberDTO>.CreateAsync(
                 query.AsNoTracking().ProjectTo<MemberDTO>(_mapper.ConfigurationProvider), 
                 userParams.PageNumber, 
