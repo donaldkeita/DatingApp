14a4d5118a76ec7a39caba95a358696f7008f399
donaldkeita <keitadonald@gmail.com>
2023-05-30 15:15:54 -0400

    Adding the like user feature - Controller methods for the like feature

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    // BaseApiController includes API controller attributes and a route that uses the controller placeholder
+
     [ServiceFilter(typeof(LogUserActivity))]
     [Route("api/[controller]")] // route : attribute ,  .../[controller] is a place holder
     [ApiController]             // ApiController : attribute
